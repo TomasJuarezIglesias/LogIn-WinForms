bc0da73aa74267117974884541b8edce86ac5cbd
Tomas Juarez <tjuarez.tm@hotmail.com>
2023-04-14 09:33:35 -0300

    Creacion de carpeta models para EntityFramework
    Se creo la base de datos y se implemento el EntityFramework, hace falta realizar los procesos de intercambio de informacion con la base de datos.

---
+++ LogIn/LogIn/Models/LoginContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogIn.Models;
+
+public partial class LoginContext : DbContext
+{
+    public LoginContext()
+    {
+    }
+
+    public LoginContext(DbContextOptions<LoginContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        => optionsBuilder.UseSqlServer("Server=TOMASJUAREZ; Database=Login; User=soporte; Password=123; TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("User");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Password).HasMaxLength(50);
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
